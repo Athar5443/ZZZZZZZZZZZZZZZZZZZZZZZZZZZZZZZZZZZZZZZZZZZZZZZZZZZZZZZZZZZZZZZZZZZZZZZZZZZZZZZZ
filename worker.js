2570197d6360a392d2942e03dcf6e5a5d3994f20
Athars <azkaathar5443@gmail.com>
2026-01-16 18:52:17 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -72,7 +72,7 @@ function renderHome() {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zzz. API Mode</title>
+    <title>Zzzz.</title>
     <style>
         * { box-sizing: border-box; }
         body {
